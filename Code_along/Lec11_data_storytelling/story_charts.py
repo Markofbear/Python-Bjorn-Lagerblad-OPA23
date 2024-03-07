4d42cd5d26a8dc6ac0a19bf8960a1607d86fb5e4
Markofbear <lagerblad.bjorn@gmail.com>
2024-03-07 10:37:43 +0100

    shift alt f update labb 2

--- Code_along/Lec11_data_storytelling/story_charts.py
+++ Code_along/Lec11_data_storytelling/story_charts.py
@@ -1,24 +1,26 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from pathlib import Path
-import pandas as pd 
+import pandas as pd
 
 # in jupyter notebook
 # DATA_PATH = "../../data/data_processing/"
 DATA_PATH = Path(__file__).parents[2] / "data" / "data_processing"
 
+
 class StoryCharts:
     def __init__(self) -> None:
         pass
 
-    def _plot(self, x, y, colors = "#0c4a6e", **label_kwargs):
+    def _plot(self, x, y, colors="#0c4a6e", **label_kwargs):
         self.fig, self.ax = plt.subplots()
 
         plt.show()
-    
-    def Line(self, x, y):
-        pass 
 
-# this is to be able to run this as a standalone script 
+    def Line(self, x, y):
+        pass
+
+
+# this is to be able to run this as a standalone script
 # the code below won't run when imported from another script
 # as __name__ will be "story_charts.py" when imported
 if __name__ == "__main__":
@@ -31,4 +33,4 @@ if __name__ == "__main__":
     print(df.head())
 
     sc = StoryCharts()
-    sc._plot(2,3)
+    sc._plot(2, 3)
